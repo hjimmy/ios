d48d59a5a7b03a86b621468964d852967ec49cb2
wolf <wolf@wolfs-Mac.local>
2016-01-13 19:50:27 +0800

    change picture and langguaes

--- automation_Test/settingsView.py
+++ automation_Test/settingsView.py
@@ -1,3 +1,4 @@
 
 addNewAccountButton_xpath = "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[2]/UIAStaticText[1]"
-addNewAccountButton_name = "Add new account"
+#addNewAccountButton_name = "Add new account"
+addNewAccountButton_name = "添加账"
